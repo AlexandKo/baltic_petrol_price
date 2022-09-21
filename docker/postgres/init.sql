2ec15f42ca21258a063626918c2680440e648d3d
Alexey Kodin <alexey.kodin@a-heads.eu>
2022-09-21 14:15:31 +0300

    updated DB schema

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,10 +1,10 @@
-\connect 'petrol_station';
+\connect 'petrol_price';
 
-GRANT ALL PRIVILEGES ON DATABASE petrol_station TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE petrol_price TO postgres;
 
-CREATE SCHEMA IF NOT EXISTS neste;
+CREATE SCHEMA IF NOT EXISTS petrol_station;
 
-CREATE TABLE IF NOT EXISTS neste.petrol_price
+CREATE TABLE IF NOT EXISTS petrol_station.neste
 (
     id                            UUID      NOT NULL PRIMARY KEY,
     created_date                  TIMESTAMP NOT NULL,
@@ -20,3 +20,17 @@ CREATE TABLE IF NOT EXISTS neste.petrol_price
     diesel_pro                    VARCHAR(10),
     diesel_pro_best_price_address TEXT
 );
+
+CREATE TABLE IF NOT EXISTS petrol_station.gotika
+(
+    id                            UUID      NOT NULL PRIMARY KEY,
+    created_date                  TIMESTAMP NOT NULL,
+    updated_date                  TIMESTAMP NOT NULL,
+    entity_version                BIGINT    NOT NULL,
+    country                       VARCHAR(2),
+    petrol                        VARCHAR(10),
+    petrol_best_price_address     TEXT,
+    diesel                        VARCHAR(10),
+    diesel_best_price_address     TEXT
+);
+
--- src/main/java/bpp/entity/GotikaPriceEntity.java
+++ src/main/java/bpp/entity/GotikaPriceEntity.java
@@ -23,8 +23,8 @@ import org.hibernate.annotations.GenericGenerator;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "neste", schema = "petrol_station")
-public class NestePriceEntity extends BaseEntity {
+@Table(name = "gotika", schema = "petrol_station")
+public class GotikaPriceEntity extends BaseEntity {
     @Id
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "uuid2")
@@ -38,34 +38,23 @@ public class NestePriceEntity extends BaseEntity {
     @Column(nullable = false)
     private String petrolBestPriceAddress;
     @Column(nullable = false)
-    private BigDecimal petrolPro;
-    @Column(nullable = false)
-    private String petrolProBestPriceAddress;
-    @Column(nullable = false)
     private BigDecimal diesel;
     @Column(nullable = false)
     private String dieselBestPriceAddress;
-    @Column(nullable = false)
-    private BigDecimal dieselPro;
-    @Column(nullable = false)
-    private String dieselProBestPriceAddress;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
-        NestePriceEntity that = (NestePriceEntity) o;
+        GotikaPriceEntity that = (GotikaPriceEntity) o;
         return id.equals(that.id) && petrol.equals(that.petrol) &&
-                petrolBestPriceAddress.equals(that.petrolBestPriceAddress) && petrolPro.equals(that.petrolPro)
-                && petrolProBestPriceAddress.equals(that.petrolProBestPriceAddress) && diesel.equals(that.diesel)
-                && dieselBestPriceAddress.equals(that.dieselBestPriceAddress) && dieselPro.equals(that.dieselPro)
-                && dieselProBestPriceAddress.equals(that.dieselProBestPriceAddress);
+                petrolBestPriceAddress.equals(that.petrolBestPriceAddress) && diesel.equals(that.diesel)
+                && dieselBestPriceAddress.equals(that.dieselBestPriceAddress);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), id, petrol, petrolBestPriceAddress, petrolPro, petrolProBestPriceAddress,
-                diesel, dieselBestPriceAddress, dieselPro, dieselProBestPriceAddress);
+        return Objects.hash(super.hashCode(), id, petrol, petrolBestPriceAddress, diesel, dieselBestPriceAddress);
     }
 }
