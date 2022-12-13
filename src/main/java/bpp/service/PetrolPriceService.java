f4acdc7673b4aa2b7824201c0bd6a942380723c9
Alexey Kodin <aleksejs.kodins@saltpay.co>
2022-12-13 16:52:31 +0200

    updated project

--- src/main/java/bpp/service/PetrolPriceService.java
+++ src/main/java/bpp/service/PetrolPriceService.java
@@ -1,11 +1,5 @@
 package bpp.service;
 
-import bpp.model.Response;
-
 public interface PetrolPriceService {
-    default boolean isResponseNull(Response<?> response) {
-        return response == null;
-    }
-
     void savePetrolPrice();
 }
